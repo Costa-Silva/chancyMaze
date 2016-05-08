fc338512db350b942cbd9a58c37b4fd9ccb83f70
Costa-Silva <antonio_costa_silva@hotmail.com>
2016-05-08 23:47:15 +0100

    almost getting main done for receiving input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here dev?
+
+
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+            String[] firstInputLine = in.readLine().split("\\s");
+
+            int numberOfRooms = Integer.parseInt(firstInputLine[0]);
+            int numberOfCorridors = Integer.parseInt(firstInputLine[1]);
+
+
+            for (int i = 0; i < numberOfCorridors ; i++) {
+                String[] allowedToMove = in.readLine().split("\\s");
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/chancyMaze.java
@@ -0,0 +1,8 @@
+/**
+ * Created by AntónioSilva on 08/05/2016.
+ */
+public class chancyMaze {
+
+
+    //TODO
+}
